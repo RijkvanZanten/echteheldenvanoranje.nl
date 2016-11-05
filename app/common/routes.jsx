13cee788548c8276ecad614f01f8d1e2bee2481b
RijkvanZanten <rijkvanzanten@me.com>
2016-11-05 14:55:18 +0100

    add homepage component

--- app/common/routes.jsx
+++ app/common/routes.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './containers/App';
+import Home from './containers/Home';
 
 const routes = (
   <Route path="/" component={App}>
-    <IndexRoute />
+    <IndexRoute component={Home}/>
   </Route>
 );
 
