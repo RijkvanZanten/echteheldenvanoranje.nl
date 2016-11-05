3ec9306e0a226459daf44b269cdc8c71bfa447cb
RijkvanZanten <rijkvanzanten@me.com>
2016-11-05 19:57:08 +0100

    add route and empty container for single event pages

---
+++ app/common/containers/Event.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Radium from 'radium';
+
+@Radium
+class Event extends Component {
+  render() {
+    return (
+      <div style={styles.container}>
+        <h1 style={styles.title}>Rijk van Zanten</h1>
+      </div>
+    );
+  }
+}
+
+const styles = {
+  container: {
+    padding: '5em'
+  },
+  title: {
+    textTransform: 'uppercase',
+    fontFamily: 'Nexa',
+    fontSize: '3em'
+  },
+};
+
+export default Event;
--- app/common/routes.jsx
+++ app/common/routes.jsx
@@ -4,11 +4,13 @@ import { Route, IndexRoute } from 'react-router';
 import App from './containers/App';
 import Home from './containers/Home';
 import Person from './containers/Person';
+import Event from './containers/Event';
 
 const routes = (
   <Route path="/" component={App}>
     <IndexRoute component={Home}/>
     <Route path="persoon/:id" component={Person}/>
+    <Route path="event/:id" component={Event}/>
   </Route>
 );
 
