d34a003eb400b4d726a370eb09f0ccb87d9fec07
RijkvanZanten <rijkvanzanten@me.com>
2016-11-05 19:55:38 +0100

    add route and empty container for single person pages

--- app/common/routes.jsx
+++ app/common/routes.jsx
@@ -3,10 +3,12 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './containers/App';
 import Home from './containers/Home';
+import Person from './containers/Person';
 
 const routes = (
   <Route path="/" component={App}>
     <IndexRoute component={Home}/>
+    <Route path="persoon/:id" component={Person}/>
   </Route>
 );
 
