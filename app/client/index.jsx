6587ffcabe1a8ce8bfc4d1087003c0cced8d4726
RijkvanZanten <rijkvanzanten@me.com>
2016-11-06 15:21:30 +0100

    remove caching

--- app/client/index.jsx
+++ app/client/index.jsx
@@ -3,11 +3,7 @@ import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
-import { persistStore, autoRehydrate } from 'redux-persist';
 import reducer from '../common/reducers';
-import localForage from 'localforage';
-import createActionBuffer from 'redux-action-buffer';
-import { REHYDRATE } from 'redux-persist/constants';
 import thunk from 'redux-thunk';
 
 import createSocketMiddleware from './socketMiddleware';
@@ -22,25 +18,14 @@ import StyleProvider from '../common/StyleProvider';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // eslint-disable-line
 
-let cachingAvailable = true;
-
-try {
-  window.localStorage;
-} catch(e) {
-  cachingAvailable = false;
-  console.warn('Caching disabled.', e); // eslint-disable-line
-}
-
 const middleware = [thunk, socketMiddleware];
-
-if(cachingAvailable) middleware.push(createActionBuffer(REHYDRATE));
 
 let enhancers;
 
 if(process.env.NODE_ENV === 'production') {
-  enhancers = compose(autoRehydrate(), applyMiddleware(...middleware));
+  enhancers = compose(applyMiddleware(...middleware));
 } else {
-  enhancers = composeEnhancers(autoRehydrate(), applyMiddleware(...middleware));
+  enhancers = composeEnhancers(applyMiddleware(...middleware));
 
   if(module.hot) {
     module.hot.accept('../common/reducers/index', () => {
@@ -52,7 +37,7 @@ if(process.env.NODE_ENV === 'production') {
 
 const store = createStore(reducer, enhancers);
 
-function renderDOM() {
+
   render(
     <StyleProvider userAgent={navigator.userAgent}>
       <Provider store={store}>
@@ -60,10 +45,3 @@ function renderDOM() {
       </Provider>
     </StyleProvider>,
   document.getElementById('app'));
-}
-
-if(cachingAvailable) {
-  persistStore(store, { storage: localForage }, renderDOM);
-} else {
-  renderDOM();
-}
