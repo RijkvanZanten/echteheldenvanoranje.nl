121b3cb069e6fe3cfed7b50b73ee96279b84f5ad
RijkvanZanten <rijkvanzanten@me.com>
2016-11-10 00:44:56 +0100

    re-enable client-side caching

--- app/client/index.jsx
+++ app/client/index.jsx
@@ -3,6 +3,10 @@ import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
+import { persistStore, autoRehydrate } from 'redux-persist';
+import localForage from 'localforage';
+import createActionBuffer from 'redux-action-buffer';
+import { REHYDRATE } from 'redux-persist/constants';
 import reducer from '../common/reducers';
 import thunk from 'redux-thunk';
 
@@ -18,14 +22,24 @@ import StyleProvider from '../common/StyleProvider';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // eslint-disable-line
 
+let cachingAvailable = true;
+try {
+  window.localStorage;
+} catch(e) {
+  cachingAvailable = false;
+  console.warn('Caching disabled. ', e); // eslint-disable-line
+}
+
 const middleware = [thunk, socketMiddleware];
+
+if(cachingAvailable) middleware.push(createActionBuffer(REHYDRATE));
 
 let enhancers;
 
 if(process.env.NODE_ENV === 'production') {
-  enhancers = compose(applyMiddleware(...middleware));
+  enhancers = compose(autoRehydrate(), applyMiddleware(...middleware));
 } else {
-  enhancers = composeEnhancers(applyMiddleware(...middleware));
+  enhancers = composeEnhancers(autoRehydrate(), applyMiddleware(...middleware));
 
   if(module.hot) {
     module.hot.accept('../common/reducers/index', () => {
@@ -37,7 +51,7 @@ if(process.env.NODE_ENV === 'production') {
 
 const store = createStore(reducer, enhancers);
 
-
+function renderDom() {
   render(
     <StyleProvider userAgent={navigator.userAgent}>
       <Provider store={store}>
@@ -45,3 +59,10 @@ const store = createStore(reducer, enhancers);
       </Provider>
     </StyleProvider>,
   document.getElementById('app'));
+}
+
+if(cachingAvailable) {
+  persistStore(store, { storage: localForage }, renderDom);
+} else {
+  renderDom();
+}
