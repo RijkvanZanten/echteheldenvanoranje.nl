2fd15fd186cb8f0dfbca775881ccb7b1b35ac345
RijkvanZanten <rijkvanzanten@me.com>
2016-11-09 14:00:11 +0100

    add category to single person page

--- app/common/containers/Person.jsx
+++ app/common/containers/Person.jsx
@@ -8,8 +8,8 @@ const mapStateToProps = function(state) {
   };
 };
 
-@Radium
 @connect(mapStateToProps)
+@Radium
 class Person extends Component {
   constructor(props) {
     super(props);
@@ -19,10 +19,23 @@ class Person extends Component {
     const id = this.props.params.id;
     const person = this.props.people[Object.keys(this.props.people).filter((d) => this.props.people[d].id == id)];
 
+    let mainCategory;
+
+    if(person.categories.indexOf('Militair') !== -1) {
+      mainCategory = 'Militair';
+    } else if(person.categories.indexOf('Verzet') !== -1) {
+      mainCategory = 'Verzet';
+    } else if(person.categories.indexOf('Sjoa') !== -1) {
+      mainCategory = 'Sjoa';
+    } else {
+      mainCategory = 'Burger';
+    }
+
     return (
       <div style={styles.container}>
-        <h1 style={styles.title}>{person.name}</h1>
-        <h2 style={styles.age}>{person.birth_day} {this.months[person.birth_month - 1]} {person.birth_year} — {person.death_day} {this.months[person.death_month - 1]} {person.death_year}</h2>
+        <h1 style={styles.title}>{person.name} <p style={[styles.category, styles[mainCategory]]}>{mainCategory}</p></h1>
+        <p style={styles.sublabel}>{person.birth_day} {this.months[person.birth_month - 1]} {person.birth_year} — {person.death_day} {this.months[person.death_month - 1]} {person.death_year} ({person.death_year - person.birth_year} jaar oud)</p>
+        <p style={styles.sublabel}>{person.place_of_birth} — {person.place_of_death}</p>
       </div>
     );
   }
@@ -35,16 +48,31 @@ const styles = {
   title: {
     textTransform: 'uppercase',
     fontFamily: 'Nexa',
-    fontSize: '3em',
+    fontSize: '2.5em',
     marginBottom: 0
   },
-  age: {
+  sublabel: {
     textTransform: 'uppercase',
     fontFamily: 'Nexa',
-    fontSize: '2em',
+    fontSize: '1.5em',
     color: '#949799',
-    marginTop: 0
-  }
+    margin: 0
+  },
+  category: {
+    display: 'inline-block',
+    fontSize: '.5em',
+    verticalAlign: 'baseline'
+  },
+  Militair: {
+    color: '#8fda62'
+  },
+  Verzet: {
+    color: '#daab62'
+  },
+  Sjoa: {
+    color: '#6e9fe9'
+  },
+  Burger: {}
 };
 
 export default Person;
